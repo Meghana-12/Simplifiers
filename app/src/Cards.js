e1744300faf90acb2c999d6ae78e1141e4fd0af4
Meghana <meghana@iitbhilai.ac.in>
2020-07-10 17:47:49 +0530

    added return statements in code.l and changes in cards

---
+++ app/src/Cards.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect }from 'react';
+import ReactDOM from 'react-dom';
+import { Grid, Box, Container } from "@material-ui/core";
+import Card from './Card';
+import BackToTop from './nav';
+import {Title} from './title';
+import Para from './Para';
+export function Cards(x) {
+
+  return ( 
+    <Grid
+    spacing={3}
+    container
+    direction="row"
+    justify="space-evenly"
+    alignItems="center"   
+  >
+    <Card
+      imgLink={x.imgLink1}
+      title={x.title1}
+      content={x.content1}
+      title_size = {x.title_size1 }
+      content_size={x.content_size1}
+      title_color={x.title_color1}
+      content_color={x.content_color1}
+      />
+     <Card 
+      imgLink={x.imgLink2}
+      title={x.title2}
+      content={x.content2}
+      title_size = {x.title_size2 }
+      content_size={x.content_size2}
+      title_color={x.title_color2}
+      content_color={x.content_color2}
+      />
+      <Card 
+      imgLink={x.imgLink3}
+      title={x.title3}
+      content={x.content3}
+      title_size = {x.title_size3 }
+      content_size={x.content_size3}
+      title_color={x.title_color3}
+      content_color={x.content_color3}
+      />
+    </Grid>    
+  );
+}
--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect }from 'react';
 import ReactDOM from 'react-dom';
 import { Grid, Box, Container } from "@material-ui/core";
-import Card from './Card';
+import {Cards} from './Cards';
 import BackToTop from './nav';
 import {Title} from './title';
 import Para from './Para';
@@ -26,67 +26,66 @@ function App() {
         title_color=""
         content_color=""
         />
-     <Grid
-          spacing={3}
-          container
-          direction="row"
-          justify="space-evenly"
-          alignItems="center"
-
-         
-        >
-          <Card 
-            imgLink=".." 
-            title="LIzard" 
-            content="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-              across all continents except Antarctica"
-            title_size = ""
-            content_size=""
-            title_color=""
-            content_color=""
-            />
-           <Card 
-            imgLink=".." 
-            title="LIzard" 
-            content="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica"
-            title_size = ""
-            content_size=""
-            title_color=""
-            content_color=""
-            />
-             <Card 
-             imgLink=".." 
-             title="LIzard" 
-             content="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica"
-            title_size = ""
-            content_size=""
-            title_color=""
-            content_color=""
-            />
-            <Card 
-            imgLink=".." 
-            title="LIzard" 
-            content="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica"
-            title_size = ""
-            content_size=""
-            title_color=""
-            content_color=""
-            />
-             <Card 
-             imgLink=".." 
-             title="LIzard" 
-             content="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica"
-            title_size = ""
-            content_size=""
-            title_color=""
-            content_color=""
-            />
-          </Grid> 
-          
+      <Cards 
+        imgLink1=".." 
+        title1="LIzard" 
+        content1="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica"
+        title_size1= ""
+        content_size1=""
+        title_color1=""
+        content_color1=""
+        imgLink2=".." 
+        title2="LIzard" 
+        content2="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica"
+        title_size2= ""
+        content_size2=""
+        title_color2=""
+        content_color2=""
+        imgLink3=".." 
+        title3="LIzard" 
+        content3="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica"
+        title_size3 = ""
+        content_size3=""
+        title_color3=""
+        content_color3=""
+      />
+          <Para 
+        title="afhjbfj" 
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
+        title_size = ""
+        content_size=""
+        title_color=""
+        content_color=""
+        />
+          <Cards 
+        imgLink1=".." 
+        title1="LIzard" 
+        content1="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica"
+        title_size1= ""
+        content_size1=""
+        title_color1=""
+        content_color1=""
+        imgLink2=".." 
+        title2="LIzard" 
+        content2="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica"
+        title_size2= ""
+        content_size2=""
+        title_color2=""
+        content_color2=""
+        imgLink3=".." 
+        title3="LIzard" 
+        content3="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica"
+        title_size3 = ""
+        content_size3=""
+        title_color3=""
+        content_color3=""
+      />
          
         </BackToTop>
         
